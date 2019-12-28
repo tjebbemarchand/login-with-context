33f5a78cbdfeb2ece63fe824a83e492410657b7e
Tjebbe <tjebbemarchand@icloud.com>
2019-12-28 19:16:40 +0100

    Fixed search icon from not showing up

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
-import SearchIcon from "@material-ui/core/AppBar";
+import SearchIcon from "@material-ui/icons/Search";
 import Switch from "@material-ui/core/Switch";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles/NavBarStyles";
