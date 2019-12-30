b38281ecb45695928e741a3c5efd668192517852
Tjebbe <tjebbemarchand@icloud.com>
2019-12-30 16:15:09 +0100

    Imported context and used it for changing themes

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,13 +8,16 @@ import SearchIcon from "@material-ui/icons/Search";
 import Switch from "@material-ui/core/Switch";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles/NavBarStyles";
+import { ThemeContext } from "./contexts/ThemeContext";
 
 class Navbar extends Component {
+    static contextType = ThemeContext;
     render() {
         const { classes } = this.props;
+        const { isDarkMode } = this.context;
         return (
             <div className={classes.root}>
-                <AppBar position="static" color="primary">
+                <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
                     <Toolbar>
                         <IconButton className={classes.menuButton} color="inherit">
                             <span>🇫🇷</span>
